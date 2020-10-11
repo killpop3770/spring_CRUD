c40c2880c5d34ed279c131ad8f521cca9bd5202f
killpop3770 <komstroj3@mail.ru>
2020-10-12 01:54:33 +0300

    first commit

---
+++ src/test/java/com/x/springboot/SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.x.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
